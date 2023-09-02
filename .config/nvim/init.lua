6e63fdc6abb00cd42d729a98c0667c3dbf1d9c8c
shyyn <106408425+shyyn@users.noreply.github.com>
2023-09-02 11:59:44 -0400

    Create init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,10 @@
+require("gh0st.plugins-setup")
+require("gh0st.core.options")
+require("gh0st.core.keymaps")
+require("gh0st.core.colorscheme")
+require("gh0st.plugins.nvim-tree")
+require("gh0st.plugins.lualine")
+require("gh0st.plugins.colorizer")
+
+vim.opt.linebreak = true
+vim.opt.wrap = true
